7572b5ee9b413a05a7b5152dfe2457092cbc3ba4
A'zamjon Abdusalomov <azamabdusalomov@icloud.com>
2025-05-23 21:23:35 +0500

    fix: prettier configuration change

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,23 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3000);
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('PORT', 3000);
 
-  // Swagger setup
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('EduMasters API')
-    .setDescription('Edumasters backend endpoints')
-    .setVersion('1.0')
-    .build();
+    // Swagger setup
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('EduMasters API')
+        .setDescription('Edumasters backend endpoints')
+        .setVersion('1.0')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
-  console.log(`🚀 App running on http://localhost:${port}`);
-  console.log(`📚 Swagger docs at http://localhost:${port}/api`);
+    await app.listen(port);
+    console.log(`🚀 App running on http://localhost:${port}`);
+    console.log(`📚 Swagger docs at http://localhost:${port}/api`);
 }
 bootstrap();
